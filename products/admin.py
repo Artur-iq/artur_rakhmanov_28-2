0dcf59e46e81ca519327414cc8f347f3f1bbf148
Artur <128506353+Artur-iq@users.noreply.github.com>
2023-05-18 23:58:19 +0600

    Add files via upload

---
+++ products/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from products.models import Products, Comments
+
+
+# Register your models here.
+
+admin.site.register(Products)
+admin.site.register(Comments)
---
+++ products/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+# Create your models here.
+class Products(models.Model):
+    image = models.ImageField(blank=True, null=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    rate = models.FloatField(default=0)
+    created_date = models.DateField(auto_now_add=True)
+    modified_date = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+
+
+class Comments(models.Model):
+    text = models.CharField(max_length=256)
+    created_data = models.DateTimeField(auto_now_add=True)
+    product = models.ForeignKey(Products, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.product.title} - {self.text}'
