531048f01b410d96548e102bfcc5b5d027bc00b9
Artur <128506353+Artur-iq@users.noreply.github.com>
2023-05-30 15:34:42 +0600

    Delete products directory

--- products/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-
-class ProductCreateForm(forms.Form):
-    title = forms.CharField(max_length=50, min_length=3)
-    image = forms.ImageField()
-    description = forms.CharField(widget=forms.Textarea, min_length=3)
-    rate = forms.IntegerField()
