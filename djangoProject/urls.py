607c67a0394ec66daaaf189fed36c62e93f17e89
Artur <128506353+Artur-iq@users.noreply.github.com>
2023-05-30 23:22:18 +0600

    Add files via upload

---
+++ djangoProject/urls.py
@@ -0,0 +1,41 @@
+"""
+URL configuration for djangoProject project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls.static import static
+
+from products.views import main_page_view, product_view, products_detail_view, products_create_vies, hello_view, \
+    now_data, goodby
+from djangoProject import settings
+from users.views import auth_view, register_view, logout_view
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', main_page_view),
+    path('products/', product_view),
+    path('products/create/', products_create_vies),
+    path('products/<int:id>/', products_detail_view),
+    path('hello/', hello_view),
+    path('now_data/', now_data),
+    path('goodby/', goodby()),
+    path('users/auth/', auth_view),
+    path('users/register/', register_view),
+    path('users/logout/', logout_view),
+
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
